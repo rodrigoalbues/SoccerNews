5e28318936abf4297767146b78b4df439dc91eaa
rodrigoalbues <95535393+rodrigoalbues@users.noreply.github.com>
2022-07-15 01:54:28 -0400

    Refactoring e evoluções arquiteturais.

---
+++ app/src/main/java/me/dio/soccernews/data/SoccerNewsRepository.java
@@ -0,0 +1,39 @@
+package me.dio.soccernews.data;
+
+import androidx.room.Room;
+
+import me.dio.soccernews.App;
+import me.dio.soccernews.data.local.SoccerNewsDb;
+import me.dio.soccernews.data.remote.SoccerNewsApi;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class SoccerNewsRepository {
+
+    private static final String REMOTE_API_URL = "https://rodrigoalbues.github.io/soccer-news-api/";
+    private static final String LOCAL_DB_NAME = "soccer news";
+
+    private SoccerNewsApi remoteApi;
+    private SoccerNewsDb localDb;
+
+    public SoccerNewsApi getRemoteApi() { return remoteApi;}
+
+    public SoccerNewsDb getLocalDb() {return localDb;}
+
+    // Singleton: garante uma instância única dos atributos relacionados ao Retrofit e Room.
+    private SoccerNewsRepository () {
+        remoteApi = new Retrofit.Builder()
+                .baseUrl(REMOTE_API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(SoccerNewsApi.class);
+
+        localDb = Room.databaseBuilder(App.getInstance(), SoccerNewsDb.class, LOCAL_DB_NAME).build();
+    }
+
+    public static SoccerNewsRepository getInstance() { return LazyHolder.INSTANCE;}
+
+    private static class LazyHolder {
+        private static final SoccerNewsRepository INSTANCE = new SoccerNewsRepository();
+    }
+}
